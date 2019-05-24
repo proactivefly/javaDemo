19e92a1706dab80caf2907f42bfebf252e031c75
ecstack <ecstack@sina.com>
2019-05-24 16:14:36 +0800

    add

--- src/demo.java
+++ src/demo.java
@@ -1,5 +1,17 @@
 public class demo {
   public static void main(String[] args) {
-    
+    System.out.print("hello world");
+  }
+  String breed;
+  int age;
+  String color;
+  void hungry(){
+
+  }
+  void barking(){
+
+  }
+  void sleeping(){
+
   }
 }
